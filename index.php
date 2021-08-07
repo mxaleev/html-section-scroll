1148d1a6c45b090b7d320b222de0cbf9af49c1b3
Mikhail Khaleev <mxaleev@gmail.com>
2021-08-07 08:35:32 +0300

    Git init

---
+++ index.php
@@ -0,0 +1,99 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- Bootstrap CSS -->
+  <link href="./css/styles.css" rel="stylesheet" crossorigin="anonymous">
+  <title>Hello, world!</title>
+</head>
+<body>
+
+<div>
+  <ul class="breadcrumbs">
+    <li class="breadcrumbs__item"><a href="/" class="breadcrumbs__link fancy-underline">Home</a></li>
+    <li class="breadcrumbs__item"><a href="/" class="breadcrumbs__link fancy-underline">Women</a></li>
+    <li class="breadcrumbs__item"><a href="/" class="breadcrumbs__link fancy-underline">Sports</a></li>
+  </ul>
+  <main class="page-main">
+    <div class="container">
+      <div class="products-list-page">
+        <div class="products-list-page__header">
+          <a href="/" class="breadcrumbs-back-button products-list-page__back-button">Back</a>
+          <div class="products-list-page__name-wrapper">
+            <h1 class="products-list-page__name">Sports</h1>
+            <span class="products-list-page__items-count">593 Items</span>
+          </div>
+
+          <div class="products-list-page__category-siblings-wrapper">
+            <div class="category-siblings">
+              <div class="category-siblings__wrapper">
+                <div class="category-siblings__slider">
+                  <ul class="category-siblings__list">
+                    <li class="category-siblings__sibling">
+                      <a href="/" class="category-siblings__sibling-link fancy-underline">
+                        Accessories
+                      </a>
+                    </li>
+                    <li class="category-siblings__sibling">
+                      <a href="/" class="category-siblings__sibling-link fancy-underline">
+                        Functional Underwear
+                      </a>
+                    </li>
+                    <li class="category-siblings__sibling">
+                      <a href="/" class="category-siblings__sibling-link fancy-underline">
+                        Trousers &amp; shorts
+                      </a>
+                    </li>
+                    <li class="category-siblings__sibling">
+                      <a href="/" class="category-siblings__sibling-link fancy-underline">
+                        Jackets &amp; cardigans
+                      </a>
+                    </li>
+                    <li class="category-siblings__sibling">
+                      <a href="/" class="category-siblings__sibling-link fancy-underline">
+                        Polos &amp; short-sleeve shirts
+                      </a>
+                    </li>
+                    <li class="category-siblings__sibling">
+                      <a href="/" class="category-siblings__sibling-link fancy-underline">
+                        Jumpers &amp; long-sleeve shirts
+                      </a>
+                    </li>
+                    <li class="category-siblings__sibling">
+                      <a href="/" class="category-siblings__sibling-link fancy-underline">
+                        Sports Bras
+                      </a>
+                    </li>
+                    <li class="category-siblings__sibling">
+                      <a href="/" class="category-siblings__sibling-link fancy-underline">
+                        Sports Socks
+                      </a>
+                    </li>
+                    <li class="category-siblings__sibling">
+                      <a href="/" class="category-siblings__sibling-link fancy-underline">
+                        Tops &amp; Tanktops
+                      </a>
+                    </li>
+                    <li class="category-siblings__sibling">
+                      <a href="/" class="category-siblings__sibling-link fancy-underline">
+                        Sale
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <span class="category-siblings__button category-siblings__button--prev" style="display: none;"></span>
+              <span class="category-siblings__button category-siblings__button--next" style=""></span>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </main>
+</div>
+
+</body>
+</html>
